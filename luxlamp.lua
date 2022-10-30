c7c14ea0a7bc79004cbba2d9dd03aad739f07b0f
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2022-10-30 18:14:18 -0500

    Changed old Luxlamps to the new Luxbulbs
    Luxlamps are now Luxbulbs, which must be directly powered by a lux reaction.

--- luxlamp.lua
+++ luxlamp.lua
@@ -1,105 +1,88 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, math
-    = minetest, nodecore, math
+local math, minetest, nodecore
+    = math, minetest, nodecore
+local math_ceil
+    = math.ceil
 -- LUALOCALS > ---------------------------------------------------------
 local modname = minetest.get_current_modname()
 ------------------------------------------------------------------------
-local function luxlamp (desc, id, power, light)
-local delum	= power-1
-local flux	= power+1
-local charge	= power*100
-local vlux	= power*60
-local time	= 1000/power
-------------------------------------------------------------------------
 local particle = "nc_lux_base.png^[mask:nc_lux_dot_mask.png^[opacity:32"
 ------------------------------------------------------------------------
-minetest.register_node(modname .. ":luxlamp_" .. id, {
-	description = desc.. " Luxlamp",
-	tiles = {
-		"nc_optics_glass_frost.png^(nc_lux_base.png^[opacity:" .. vlux .. ")^(nc_lode_annealed.png^[mask:" .. modname .. "_mask_lamp_top.png)",
-		"nc_lode_annealed.png",
-		"nc_optics_glass_frost.png^(nc_lux_base.png^[opacity:" .. vlux .. ")^(nc_lode_annealed.png^[mask:" .. modname .. "_mask_lamp_side.png)",
-	},
-	drawtype = "nodebox",
-	paramtype = "light",
-	light_source = light,
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.3125, -0.3125, -0.3125, 0.3125, 0.25, 0.3125},	-- Core
-			{-0.5, -0.375, -0.5, 0.5, -0.25, 0.5},				-- Plate_Large
-			{-0.375, -0.4375, -0.375, 0.375, -0.375, 0.375},		-- Plate_Med
-			{-0.25, -0.5, -0.25, 0.25, -0.4375, 0.25},			-- Plate_Small
-			{-0.375, 0.25, -0.375, 0.375, 0.3125, 0.375},		-- Cap_Large
-			{-0.25, 0.3125, -0.25, 0.25, 0.375, 0.25},			-- Cap_Med
-			{-0.125, 0.375, -0.125, 0.125, 0.4375, 0.125},		-- Cap_Small
-			{-0.0625, 0.4375, -0.0625, 0.0625, 0.5, 0.0625},		-- Cap_Peak
-		}
-	},
-	sunlight_propagates = true,
-	groups = {
-		stack_as_node = 1,
-		snappy = 1,
-		luxlamp = 1
-	},
-	stack_max = 1,
-	sounds = nodecore.sounds("nc_optics_glassy"),
-})
----------------------------------------
------------Luxlamp Recycling-----------
-nodecore.register_craft({
-		label = "break luxlamp apart",
-		action = "pummel",
-		toolgroups = {choppy = 1, thumpy = 1, cracky = 1},
-		nodes = {
-			{
-				match = {groups = {luxlamp = true}},
-				replace = "nc_optics:glass_crude"
+local function bulb(charge)
+local vlux	= charge*60
+	return minetest.register_node(modname .. ":luxlamp_" .. charge, {
+		description = "Luxbulb",
+		drawtype = "nodebox",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},				-- AnchorPlate
+				{-0.4375, -0.5, -0.4375, 0.4375, -0.3125, 0.4375},	-- BasePlate
+				{-0.3125, -0.375, -0.3125, 0.3125, 0.25, 0.3125},		-- SmallPlate
+				{-0.375, 0.25, -0.375, 0.375, 0.3125, 0.375},		-- Crown
+				{-0.25, 0.25, -0.25, 0.25, 0.375, 0.25},			-- PeakLg
+				{-0.125, 0.3125, -0.125, 0.125, 0.4375, 0.125},		-- PeakMd
+				{-0.0625, 0.3125, -0.0625, 0.0625, 0.5, 0.0625},		-- PeakSm
 			}
 		},
-		items = {
-			{name = "nc_lode:rod_tempered", count = 4, scatter = 4},
-			{name = "nc_lux:flux_flowing", count = 10, scatter = 4}
+		paramtype = "light",
+		paramtype2 = "wallmounted",
+		tiles = {
+			"nc_optics_glass_frost.png^(nc_lux_base.png^[opacity:" .. vlux .. ")^(nc_lode_annealed.png^[mask:" .. modname .. "_mask_lamp_top.png)",
+			"nc_lode_annealed.png",
+			"nc_optics_glass_frost.png^(nc_lux_base.png^[opacity:" .. vlux .. ")^(nc_lode_annealed.png^[mask:" .. modname .. "_mask_lamp_side.png)",
 		},
-		itemscatter = 4
+		backface_culling = true,
+		use_texture_alpha = "clip",
+		groups = {
+			luxlamp = 1,
+			snappy = 1,
+			lux_emit = charge,
+			stack_as_node = 1,
+			
+		},
+		stack_max = 1,
+		light_source = charge * 6,
+		sounds = nodecore.sounds("nc_optics_glassy"),
+		drop = modname.. ":luxlamp_0"
 	})
-
----------------------------------------
-----------Luxlamp Diminishing----------
-nodecore.register_abm({
-		label = "Luxlamp Delumination",
-		interval = charge,
-		chance = 1,
-		nodenames = {modname .. ":luxlamp_" .. id},
-		action = function(pos)
-			if minetest.find_node_near(pos,
-				1, {"nc_lux:flux_flowing"}) then
-				return false
-			end
-			if power > 0 then
-				return minetest.set_node(pos, {name = modname .. ":luxlamp_" .. delum})
-			end
-		end
-	})
----------------------------------------
-nodecore.register_aism({
-		label = "Held Delumination",
-		interval = charge,
-		chance = 1,
-		itemnames = {modname .. ":luxlamp_" .. id},
-		action = function(stack, data)
-			if power > 0 then
-				stack:set_name(modname .. ":luxlamp_" .. delum)
-				return stack
-			end
-		end
-	})
-
-----------------------------------------
 end
-luxlamp("Dull",		"0",	0,	1)
-luxlamp("Dim",			"1",	1,	4)
-luxlamp("Lambent",		"2",	2,	8)
-luxlamp("Radiant",		"3",	3,	12)
-luxlamp("Brilliant",	"4",	4,	16)
-luxlamp("Fulgent",		"5", 5,	20)
+------------------------------------------------------------------------
+for i = 0, 3 do bulb(i) end
+------------------------------------------------------------------------
+nodecore.register_abm({
+		label = "Illuminate Bulb",
+		nodenames = {modname.. ":luxlamp_0"},
+		neighbors = {"group:lux_cobble"},
+		interval = 2,
+		chance = 2,
+		action = function(pos, node)
+		node.name = modname.. ":luxlamp_1"
+		minetest.set_node(pos, node)
+		end
+	})
+nodecore.register_abm({
+		label = "Superluminate Bulb",
+		nodenames = {modname.. ":luxlamp_1"},
+		neighbors = {"nc_lux:cobble3"},
+		interval = 10,
+		chance = 1,
+		action = function(pos, node)
+		node.name = modname.. ":luxlamp_2"
+		minetest.set_node(pos, node)
+		end
+	})	
+nodecore.register_abm({
+		label = "Hyperluminate Bulb",
+		nodenames = {modname.. ":luxlamp_2"},
+		neighbors = {"nc_lux:cobble5"},
+		interval = 10,
+		chance = 1,
+		action = function(pos, node)
+		node.name = modname.. ":luxlamp_3"
+		minetest.set_node(pos, node)
+		end
+	})	
+
+------------------------------------------------------------------------
+
