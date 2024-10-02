b0917e8a22ec65ae41abf91b7d75b8dc1994040b
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-01 19:53:47 -0500

    made chip&lode sparking more feasible

--- sparks.lua
+++ sparks.lua
@@ -12,8 +12,9 @@ nodecore.register_craft({
 			{match = {groups = {chisel = true}}}
 		},
 		consumewield = 1,
-		duration = 1,
+		duration = -1,
 		after = function(pos)
-			return nodecore.firestick_spark_ignite(pos,true)
+			nodecore.firestick_spark_ignite(pos,true)
+			nodecore.sound_play("nc_api_toolbreak", {pos = pos, gain = 1})
 		end
 	})
